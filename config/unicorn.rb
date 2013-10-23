ef99810705379b74aefe40f44b14ca856b30a158
hdaoudi <hd@venturiate.com>
2013-10-23 14:05:24 +0200

    update-v0.2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,46 +1,25 @@
-env = ENV["RAILS_ENV"] || "development"
-
 worker_processes 4
-
-# This loads the application in the master process before forking
-# worker processes
-# Read more about it here:
-# http://unicorn.bogomips.org/Unicorn/Configurator.html
 preload_app true
+timeout 60
 
-timeout 30
-
-# This is where we specify the socket.
-# We will point the upstream Nginx module to this socket later on
-listen "/home/hd/touzine/current/tmp/sockets/unicorn.sock", :backlog => 64
-
-pid "/home/hd/touzine/current/tmp/pids/unicorn.pid"
+working_directory "/home/hd/touzine/current"
+listen "/tmp/sockets/unicorn.sock", :backlog => 64
+pid "/tmp/pids/unicorn.pid"
 
 # Production specific settings
 if env == "production"
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
-  working_directory "/home/hd/touzine/current"
-
-  # feel free to point this anywhere accessible on the filesystem
   user 'deployer', 'deployers'
   shared_path = "/home/hd/touzine/shared"
-
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
   stdout_path "#{shared_path}/log/unicorn.stdout.log"
 end
 
 before_fork do |server, worker|
-# This option works in together with preload_app true setting
-# What is does is prevent the master process from holding
-# the database connection
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-# Here we are establishing the connection after forking worker
-# processes
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
